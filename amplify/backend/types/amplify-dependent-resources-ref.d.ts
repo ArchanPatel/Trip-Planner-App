2d80ff1a64d55ecf8bc8c065810f38e50776a558
archanmehulpatel@gmail.com <archanmehulpatel@gmail.com>
2023-01-18 17:26:41 -0500

    initial file push

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifytripsplanner": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifytripsplanner74e38b64": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "testImage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
